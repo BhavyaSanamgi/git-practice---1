772f1f4538342d8508ddcc382aea8ae7ca82febc
navieaops-sec <navyakumar8121@gmail.com>
2026-06-05 16:23:35 +0530

    Modified file from bhavya branch

--- test1.py
+++ test1.py
@@ -4,3 +4,8 @@ def sum_of_three_numbers(a, b, c):
 # Example usage:
 result = sum_of_three_numbers(5, 10, 20)
 print("Sum:", result)
+#mofiying bhavya code
+print("Add:",result)
+djndjfkfmkgmkg
+jfjfkffffffffff
+Thanks for the git lecture bhavya
